b89328f4a667fd22643d4c4480d845d7e8337ee2
khusaini <@muhammadkhusaini599>
2022-03-18 11:19:36 +0700

    ListOF

--- src/main/kotlin/7_List.kt
+++ src/main/kotlin/7_List.kt
@@ -4,7 +4,7 @@ fun main(args:Array<String>){
     println("Third element: ${numbers.get(2)}")
     println("Fourth element: ${numbers[3]}")
     println("Index of element \"two\" ${numbers.indexOf("two")}")
-    //List elements (including nulls) can duplicate
+    //List elements (including nulls) can duplicate.
     val bob = Person("Bob", 31)
     val people = listOf<Person>(Person("Adam", 20), bob, bob)
     val people2 = listOf<Person>(Person("Adam", 20), Person("Bob", 31), bob)
